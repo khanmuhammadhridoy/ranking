932879f7f5000353bc82ffdb1ebd929754f290df
Hridoy Khan <68956850+khanmuhammadhridoy@users.noreply.github.com>
2020-11-29 13:48:10 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".owl-carousel ").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout:1500,
+        autoplayHoverPause: true,
+    });
+});
